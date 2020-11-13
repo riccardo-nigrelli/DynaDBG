df9f388dc299cfa218a929edeaf7932d7d3323a5
riccardo-nigrelli <pentagonswithoutedges@gmail.com>
2020-11-13 23:34:38 +0100

    Roll functions return class reference, implemented to_string method

--- src/kmer.hpp
+++ src/kmer.hpp
@@ -2,11 +2,12 @@
   #define _KMER_T_HPP_
 
   #include <iostream>
+  #include <bitset>
 
   template <typename T>
   class kmer_t {
 
-    static constexpr T encoder[128] = {
+    static constexpr uint8_t encoder[128] = {
       4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
       4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
       4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
@@ -35,18 +36,35 @@
         return (value == kmer.value);
       }
 
-      void roll_left(const unsigned char _char) {
+      kmer_t& roll_left(const unsigned char _char) {
         if (encoder[_char] == 4) exit(EXIT_FAILURE);
         value = (value << 2) | encoder[_char];
+
+        return *this;
       }
 
-      void roll_right(const unsigned char _char) {
+      kmer_t& roll_right(const unsigned char _char) {
         if (encoder[_char] == 4) exit(EXIT_FAILURE);
-        value = (value >> 2) | (encoder[_char] << (8 * sizeof(T) - 4));
+        value = (value >> 2) | ((T) encoder[_char] << (8 * sizeof(T) - 4));
+
+        return *this;
       }
 
       std::string to_string() {
-        
+        std::string kmer;
+        std::string bits = std::bitset<8 * sizeof(T) - 2>(value).to_string();
+
+        for (size_t i = 0; i < bits.length(); i += 2) {
+          uint8_t by_val = (uint8_t)(std::bitset<2>(bits.substr(i, 2)).to_ulong());
+          auto index = std::find(std::begin(encoder), std::end(encoder), by_val);
+
+          if (index != std::end(encoder)) {
+            kmer.push_back((char)std::distance(encoder, index));
+          }
+          else std::cerr << "ERROR: element not found" << std::endl;
+        }
+
+        return kmer;
       }
   };
 
