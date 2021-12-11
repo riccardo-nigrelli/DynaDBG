472315d46b61b85f820ce17396d5806a0b036074
riccardo-nigrelli <pentagonswithoutedges@gmail.com>
2021-12-11 18:21:11 +0100

    Inibited OMP

--- include/pgm/piecewise_linear_model.hpp
+++ include/pgm/piecewise_linear_model.hpp
@@ -25,13 +25,13 @@
 #include <utility>
 #include <vector>
 
-#ifdef _OPENMP
-#include <omp.h>
-#else
-#pragma message ("Compilation with -fopenmp is optional but recommended")
+// #ifdef _OPENMP
+// #include <omp.h>
+// #else
+// #pragma message ("Compilation with -fopenmp is optional but recommended")
 #define omp_get_num_procs() 1
 #define omp_get_max_threads() 1
-#endif
+// #endif
 
 namespace pgm::internal {
 
